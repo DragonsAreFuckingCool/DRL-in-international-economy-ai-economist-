c3c00133ebc22c4164488d5b1a3d531d9f4dea0c
Katja Milvang-Jensen <katja@milvang-jensen.dk>
2026-02-21 20:31:27 +0100

    Calls new base_env instead of original

--- ai_economist/foundation/scenarios/simple_wood_and_stone/layout_from_file.py
+++ ai_economist/foundation/scenarios/simple_wood_and_stone/layout_from_file.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import numpy as np
 from scipy import signal
 
-from ai_economist.foundation.base.base_env_original import BaseEnvironment, scenario_registry
+from ai_economist.foundation.base.base_env import BaseEnvironment, scenario_registry
 from ai_economist.foundation.scenarios.utils import rewards, social_metrics
 
 
