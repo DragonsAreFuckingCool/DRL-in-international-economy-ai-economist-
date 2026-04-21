7dff084a1c61ff2d9ec6e2182e7efa8a7800375c
Katja Milvang-Jensen <katja@milvang-jensen.dk>
2026-04-21 10:08:04 +0200

    smaller worker sizer for HPC

--- tutorials/simulation_run.py
+++ tutorials/simulation_run.py
@@ -7,7 +7,7 @@ ray.shutdown()
 ray.init(
     ignore_reinit_error=True,
     log_to_driver=False,
-    num_cpus=16,   # set to what the scheduler gave you
+    num_cpus=16, 
     num_gpus=1,
 )
 settings = exp.ExperimentSettings(
@@ -23,26 +23,26 @@ settings = exp.ExperimentSettings(
 
 
 # same as AI economist original 
-    num_workers=15,
-    num_envs_per_worker=2,
-    num_gpus=1,
-    num_cpus_per_worker= 1,
-    rollout_fragment_length=100,
-    train_batch_size=6000,      # 15 workers * 2 envs * 200 steps
-    sgd_minibatch_size=1500,
-    num_sgd_iter=4,    
+    # num_workers=15,
+    # num_envs_per_worker=2,
+    # num_gpus=1,
+    # num_cpus_per_worker= 1,
+    # rollout_fragment_length=200,
+    # train_batch_size=6000,      # 15 workers * 2 envs * 200 steps
+    # sgd_minibatch_size=1500,
+    # num_sgd_iter=4,    
 
 
 # alternative 
-    # num_workers=8, #parallel rollout processes
-    # num_envs_per_worker=2, #environments internally (vectorized environments).
-    # num_gpus=1,
-    # num_cpus_per_worker= 1,
-    # rollout_fragment_length=100, #steps each environment produces before sending data back - gives one planner decision per fragment
-    # train_batch_size=3200,      # total number of transitions collected before one training update: 8 workers * 2 envs * 200 steps
-    # sgd_minibatch_size=512, #how the training batch is split during optimization:
-    # #gradient updates on each mini-batch, where number of minibatches per iteration: train_batch_size/sgd_minibatch_size
-    # num_sgd_iter=4, #How many times PPO reuses the same batch.
+    num_workers=8, #parallel rollout processes
+    num_envs_per_worker=2, #environments internally (vectorized environments).
+    num_gpus=1,
+    num_cpus_per_worker= 1,
+    rollout_fragment_length=100, #steps each environment produces before sending data back - gives one planner decision per fragment
+    train_batch_size=3200,      # total number of transitions collected before one training update: 8 workers * 2 envs * 200 steps
+    sgd_minibatch_size=800, #how the training batch is split during optimization:
+    #gradient updates on each mini-batch, where number of minibatches per iteration: train_batch_size/sgd_minibatch_size
+    num_sgd_iter=4, #How many times PPO reuses the same batch.
 )
 
 results = exp.run_experiment(settings)
