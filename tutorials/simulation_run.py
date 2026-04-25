e68cb14bedeb6ddf85bc08ded8799c04e0343428
Katja Milvang-Jensen <katja@milvang-jensen.dk>
2026-04-25 15:12:39 +0200

    trade - no travel

--- tutorials/simulation_run.py
+++ tutorials/simulation_run.py
@@ -22,8 +22,8 @@ settings = exp.ExperimentSettings(
     save_results=True,
     travel_enabled_phase3a=False,
     travel_enabled_phase3b=False,
-    restrict_trade_to_region = True,
-    experiment_extra_tag = "Original",
+    restrict_trade_to_region = False,
+    experiment_extra_tag = "with trade no travel",
 
 
 # same as AI economist original 
