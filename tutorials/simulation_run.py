23ee7e53884a00cd2b425098bd067f947e7ec515
Katja Milvang-Jensen <katja@milvang-jensen.dk>
2026-05-09 10:23:50 +0200

    middle travel cost

--- tutorials/simulation_run.py
+++ tutorials/simulation_run.py
@@ -26,14 +26,15 @@ settings = exp.ExperimentSettings(
 
     fixed_tax_planner_id=None,
     fixed_tax_bracket_rates=(0.00, 0.095, 0.15, 0.23, 0.32, 0.395, 0.42),
-    experiment_extra_tag="higher_travel_cost",
+    experiment_extra_tag="middle_cost",
     layout_file = "stacked_51x25_symetric_original.txt",
 
-    travel_cost_coin_phase3a = 15,
-    travel_cost_labor_phase3a = 25,
+    travel_cost_coin_phase3a = 10,
+    travel_cost_labor_phase3a = 13,
 
-    travel_cost_coin_phase3b = 15,
-    travel_cost_labor_phase3b = 25,
+    travel_cost_coin_phase3b = 10,
+    travel_cost_labor_phase3b = 18,
+
 # same as AI economist original 
     # num_workers=15,
     # num_envs_per_worker=2,
