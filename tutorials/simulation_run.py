525e3e4b9c16752faf47614a783ad783344a64c2
Katja Milvang-Jensen <katja@milvang-jensen.dk>
2026-05-23 19:39:25 +0200

    travel_2_2_seed3_short

--- tutorials/simulation_run.py
+++ tutorials/simulation_run.py
@@ -46,7 +46,7 @@ settings = exp.ExperimentSettings(
     fixed_tax_bracket_rates_top=(0.05, 0.07, 0.17, 0.19, 0.27, 0.30, 0.32),
     #fixed_tax_bracket_rates_top=(0.09, 0.123, 0.18, 0.24, 0.30, 0.36, 0.405),
     fixed_tax_bracket_rates_bottom=(0.1, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37), #US
-    experiment_extra_tag="travel_2_2_seed2_short",
+    experiment_extra_tag="travel_2_2_seed3_short",
     layout_file = "stacked_51x25_symetric_original.txt",
 
     travel_cost_coin_phase3a = 2,
